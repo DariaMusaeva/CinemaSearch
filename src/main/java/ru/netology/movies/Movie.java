1eec98093387cd39e0c68788a7d5dc7cc93732d9
Mel66292 <mel57792@yandex.ru>
2023-06-13 14:24:39 +0500

    Little fix

--- src/main/java/ru/netology/movies/Movie.java
+++ src/main/java/ru/netology/movies/Movie.java
@@ -2,10 +2,10 @@ package ru.netology.movies;
 
 public class Movie {
 
-    private final int id;
-    private final String movieName;
-    private final int releaseDate;
-    private final String genre;
+    private int id;
+    private String movieName;
+    private int releaseDate;
+    private String genre;
 
     public Movie(int id, String movieName, int releaseDate, String genre) {
         this.id = id;
